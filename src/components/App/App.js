a98f780ef1161d39fcca550318b6a62495a320d9
mes32 <stockman.mike@gmail.com>
2019-05-31 15:09:03 -0500

    Rename ListShiftsPage to ShiftSchedulePage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ import {
 import './App.css';
 import AddShiftPage from '../AddShiftPage/AddShiftPage';
 import Footer from '../Footer/Footer';
-import ListShiftsPage from '../ListShiftsPage/ListShiftsPage';
+import ShiftSchedulePage from '../ShiftSchedulePage/ShiftSchedulePage';
 import Navbar from '../Navbar/Navbar';
 
 function App() {
@@ -18,10 +18,10 @@ function App() {
             <div>
                 <Navbar />
                 <Switch>
-                    <Redirect exact from="/" to="/list_shifts" />
+                    <Redirect exact from="/" to="/shift_schedule" />
                     <Route
-                        exact path="/list_shifts"
-                        component={ListShiftsPage}
+                        exact path="/shift_schedule"
+                        component={ShiftSchedulePage}
                     />
                     <Route
                         exact path="/add_shift"
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <div>
-            <Link to='/list_shifts'>List Shifts</Link> 
+            <Link to='/shift_schedule'>Shift Schedule</Link> 
             <Link to='/add_shift'>Add Shift</Link>
         </div>
     );
